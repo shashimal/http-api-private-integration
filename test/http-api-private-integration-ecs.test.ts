389a51772b698ca23be4441ce7c10b243752c0c5
Duleendra Shashimal <duleendra@bambu.co>
2022-04-10 01:09:20 +0800

    Initial commit

---
+++ test/http-api-private-integration-ecs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HttpApiPrivateIntegrationEcs from '../lib/http-api-private-integration-ecs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/http-api-private-integration-ecs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HttpApiPrivateIntegrationEcs.HttpApiPrivateIntegrationEcsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
